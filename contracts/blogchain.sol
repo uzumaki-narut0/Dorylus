221dc633bc99c629a3748e2940eea1f4dbe44022
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2018-04-15 19:58:42 +0530

    contract communicating properly via web3.js

--- contracts/blogchain.sol
+++ contracts/blogchain.sol
@@ -2,22 +2,33 @@ pragma solidity ^0.4.0;
 contract blogChainContract{
     
     mapping (string => address) content_creator_mapping;
+    string[] content_ref_arr;
     address creator_address;
+    
     function blogChainContract()public  payable{
         
     }
+    
     function bind(string content_ref) public{
         content_creator_mapping[content_ref] = msg.sender;
+        content_ref_arr.push(content_ref);
     }
     
-    function getCreatorAddress(string content_ref) public returns (address){
+    function getCreatorAddress(string content_ref) public view returns (address){
         return content_creator_mapping[content_ref];
     }
     
     function payCreator(string content_ref)public  payable{
-        // bindContentWithCreatorContract obj = new bindContentWithCreatorContract();
         creator_address = getCreatorAddress(content_ref);
         creator_address.transfer(msg.value);
     }
+    
+    function getArrayLength()public view returns (uint256){
+        return content_ref_arr.length;
+    }
+    
+    function getContentRef(uint256 idx) public view returns(string)
+    {
+        return content_ref_arr[idx];
+    }
 }
-
